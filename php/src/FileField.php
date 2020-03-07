b420936c34e8d372b6e491902a7229ca13badcd7
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-03-07 23:06:38 +0800

    fixed license name;change namespace;

---
+++ php/src/FileField.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AlibabaCloud\Tea\FileForm;
+
+use AlibabaCloud\Tea\Model;
+
+class FileField extends Model
+{
+    public $filename;
+    public $contentType;
+    public $content;
+
+    public function __construct()
+    {
+        $this->_required = [
+            "filename"    => true,
+            "contentType" => true,
+            "content"     => true
+        ];
+    }
+}

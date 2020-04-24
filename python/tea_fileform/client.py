43291455c5552e2a03ac3a706aff4cde428a2aaa
824750130 <e824750130@qq.com>
2020-04-24 15:47:26 +0800

    add python tea-fileform

---
+++ python/tea_fileform/client.py
@@ -0,0 +1,42 @@
+import random
+
+
+class FileField:
+    def __init__(self, filename, content_type, content):
+        self.filename = filename
+        self.content_type = content_type
+        self.content = content
+
+
+def get_boundary():
+    result = '%s' % int(random.random() * 100000000000000)
+    return result.zfill(14)
+
+
+def to_file_from(form, boundary):
+    form_str = ''
+    forms = {}
+    files = {}
+    for k, v in form.items():
+        if isinstance(v, FileField):
+            file_field = {
+                'filename':  v.filename,
+                'content_type': v.content_type,
+                'content': v.content
+            }
+            files[k] = file_field
+        else:
+            forms[k] = v
+    for k, v in forms.items():
+        # handle str
+        form_str += '--%s\r\nContent-Disposition: form-data; name=\"%s\"\r\n\r\n%s\r\n' %\
+                    (boundary, k, v)
+    for k, v in files.items():
+        # handle file object
+        start = '--%s\r\nContent-Disposition: form-data; name=\"%s\";' % (boundary, k)
+        content = ' filename: \"%s\"\r\nContent-Type: %s\r\n\r\n%s\r\n' % (v['filename'], v['content_type'], v['content'].read())
+        content = start + content
+        form_str += content
+    form_str += '--{}--\r\n'.format(boundary)
+    return form_str.encode('utf-8')
+

283fbedef0aa799da81df13c67b21a01eb12ceb8
824750130 <e824750130@qq.com>
2020-05-09 10:12:33 +0800

    add models and fix static method call rule

---
+++ python/alibabacloud_tea_fileform/models.py
@@ -0,0 +1,5 @@
+class FileField:
+    def __init__(self, filename, content_type, content):
+        self.filename = filename
+        self.content_type = content_type
+        self.content = content

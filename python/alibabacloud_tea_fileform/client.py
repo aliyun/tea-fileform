7237f2947e76624d8c7d8407cbc4b514c35cdcae
824750130 <e824750130@qq.com>
2020-05-09 10:12:33 +0800

    add models and fix static method call rule

--- python/alibabacloud_tea_fileform/client.py
+++ python/alibabacloud_tea_fileform/client.py
@@ -1,42 +1,39 @@
 import random
 
-
-class FileField:
-    def __init__(self, filename, content_type, content):
-        self.filename = filename
-        self.content_type = content_type
-        self.content = content
+from alibabacloud_tea_fileform.models import FileField
 
 
-def get_boundary():
-    result = '%s' % int(random.random() * 100000000000000)
-    return result.zfill(14)
+class Client:
+    @staticmethod
+    def get_boundary():
+        result = '%s' % int(random.random() * 100000000000000)
+        return result.zfill(14)
 
-
-def to_file_from(form, boundary):
-    form_str = ''
-    forms = {}
-    files = {}
-    for k, v in form.items():
-        if isinstance(v, FileField):
-            file_field = {
-                'filename':  v.filename,
-                'content_type': v.content_type,
-                'content': v.content
-            }
-            files[k] = file_field
-        else:
-            forms[k] = v
-    for k, v in forms.items():
-        # handle str
-        form_str += '--%s\r\nContent-Disposition: form-data; name=\"%s\"\r\n\r\n%s\r\n' %\
-                    (boundary, k, v)
-    for k, v in files.items():
-        # handle file object
-        start = '--%s\r\nContent-Disposition: form-data; name=\"%s\";' % (boundary, k)
-        content = ' filename: \"%s\"\r\nContent-Type: %s\r\n\r\n%s\r\n' % (v['filename'], v['content_type'], v['content'].read())
-        content = start + content
-        form_str += content
-    form_str += '--{}--\r\n'.format(boundary)
-    return form_str.encode('utf-8')
+    @staticmethod
+    def to_file_from(form, boundary):
+        form_str = ''
+        forms = {}
+        files = {}
+        for k, v in form.items():
+            if isinstance(v, FileField):
+                file_field = {
+                    'filename':  v.filename,
+                    'content_type': v.content_type,
+                    'content': v.content
+                }
+                files[k] = file_field
+            else:
+                forms[k] = v
+        for k, v in forms.items():
+            # handle str
+            form_str += '--%s\r\nContent-Disposition: form-data; name=\"%s\"\r\n\r\n%s\r\n' %\
+                        (boundary, k, v)
+        for k, v in files.items():
+            # handle file object
+            start = '--%s\r\nContent-Disposition: form-data; name=\"%s\";' % (boundary, k)
+            content = ' filename: \"%s\"\r\nContent-Type: %s\r\n\r\n%s\r\n' % (v['filename'], v['content_type'], v['content'].read())
+            content = start + content
+            form_str += content
+        form_str += '--{}--\r\n'.format(boundary)
+        return form_str.encode('utf-8')
 
--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -1,20 +1,21 @@
 import unittest
-from alibabacloud_tea_fileform import client
+from alibabacloud_tea_fileform.client import Client
+from alibabacloud_tea_fileform.models import FileField
 
 
 class TestClient(unittest.TestCase):
     def test_get_boundary(self):
-        boundary = client.get_boundary()
+        boundary = Client.get_boundary()
         self.assertEqual(14, len(boundary))
 
     def test_to_file_from(self):
-        body = client.to_file_from({}, 'boundary')
+        body = Client.to_file_from({}, 'boundary')
         self.assertEqual('--boundary--\r\n'.encode(), body)
 
         form = {
             'stringkey': 'string'
         }
-        body = client.to_file_from(form, 'boundary')
+        body = Client.to_file_from(form, 'boundary')
         content = "--boundary\r\n" +\
                 "Content-Disposition: form-data; name=\"stringkey\"\r\n\r\n" +\
                 "string\r\n" +\
@@ -22,17 +23,17 @@ class TestClient(unittest.TestCase):
         self.assertEqual(content.encode(), body)
 
         f = open('test_file.json', encoding='utf-8')
-        file_field = client.FileField(
+        file_field = FileField(
             filename='test_file.json',
             content_type='application/json',
             content=f
         )
-        boundary = client.get_boundary()
+        boundary = Client.get_boundary()
         form = {
             'stringkey': 'string',
             'filefield': file_field
         }
-        body = client.to_file_from(form, boundary)
+        body = Client.to_file_from(form, boundary)
         f.close()
         content = f"--{boundary}\r\n" +\
             "Content-Disposition: form-data; name=\"stringkey\"\r\n\r\n" +\

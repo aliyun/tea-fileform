9d85549b0fe0cd136babf49a2ed5cd68c9605b52
824750130 <e824750130@qq.com>
2020-05-15 17:16:11 +0800

    Add input stream

--- python/alibabacloud_tea_fileform/client.py
+++ python/alibabacloud_tea_fileform/client.py
@@ -1,6 +1,6 @@
 import random
 
-from alibabacloud_tea_fileform.models import FileField
+from alibabacloud_tea_fileform.file_form import FileFormInputStream
 
 
 class Client:
@@ -11,29 +11,4 @@ class Client:
 
     @staticmethod
     def to_file_from(form, boundary):
-        form_str = ''
-        forms = {}
-        files = {}
-        for k, v in form.items():
-            if isinstance(v, FileField):
-                file_field = {
-                    'filename':  v.filename,
-                    'content_type': v.content_type,
-                    'content': v.content
-                }
-                files[k] = file_field
-            else:
-                forms[k] = v
-        for k, v in forms.items():
-            # handle str
-            form_str += '--%s\r\nContent-Disposition: form-data; name=\"%s\"\r\n\r\n%s\r\n' %\
-                        (boundary, k, v)
-        for k, v in files.items():
-            # handle file object
-            start = '--%s\r\nContent-Disposition: form-data; name=\"%s\";' % (boundary, k)
-            content = ' filename: \"%s\"\r\nContent-Type: %s\r\n\r\n%s\r\n' % (v['filename'], v['content_type'], v['content'].read())
-            content = start + content
-            form_str += content
-        form_str += '--{}--\r\n'.format(boundary)
-        return form_str.encode('utf-8')
-
+        return FileFormInputStream(form, boundary)

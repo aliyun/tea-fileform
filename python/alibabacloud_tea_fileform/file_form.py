34b187b5e3fc24bf4678dabf6da95a413009ec45
824750130 <e824750130@qq.com>
2020-05-19 09:34:40 +0800

    File form stream object support .read()

--- python/alibabacloud_tea_fileform/file_form.py
+++ python/alibabacloud_tea_fileform/file_form.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from alibabacloud_tea_fileform.models import FileField
 
@@ -13,14 +14,14 @@ class FileFormInputStream:
         self.forms = {}
         self.files = {}
         self.files_keys = []
-        self._to_map(form)
+        self._to_map()
 
         self.form_str = ''
         self._build_str_forms()
         self.str_length = len(self.form_str)
 
-    def _to_map(self, form):
-        for k, v in form.items():
+    def _to_map(self):
+        for k, v in self.form.items():
             if isinstance(v, FileField):
                 self.files[k] = v
                 self.files_keys.append(k)
@@ -51,11 +52,69 @@ class FileFormInputStream:
         return self
 
     def __next__(self):
-        if not self.files_keys and not self.form_str:
-            raise StopIteration
+        return self.read(self.size, loop=True)
 
-        # size left
-        size = self.size
+    def file_str(self, size):
+        # handle file object
+        form_str = ''
+        start_fmt = '--%s\r\nContent-Disposition: form-data; name="%s";'
+        content_fmt = ' filename="%s"\r\nContent-Type: %s\r\n\r\n%s'
+
+        if self.file_size_left:
+            for key in self.files_keys[:]:
+                if size <= 0:
+                    break
+                file_field = self.files[key]
+                file_content = file_field.content.read(size)
+                if isinstance(file_content, bytes):
+                    file_content = file_content.decode('utf-8')
+
+                if self.file_size_left <= size:
+                    form_str += '%s\r\n' % file_content
+                    self.file_size_left = 0
+                    size -= len(file_content)
+                    self.files_keys.remove(key)
+                else:
+                    form_str += file_content
+                    self.file_size_left -= size
+                    size -= len(file_content)
+        else:
+            for key in self.files_keys[:]:
+                if size <= 0:
+                    break
+                file_field = self.files[key]
+                file_size = os.path.getsize(file_field.content.name)
+                self.file_size_left = file_size
+                file_content = file_field.content.read(size)
+                if isinstance(file_content, bytes):
+                    file_content = file_content.decode('utf-8')
+
+                # build form_str
+                start = start_fmt % (self.boundary, key)
+                content = content_fmt % (
+                    file_field.filename, file_field.content_type, file_content)
+                if self.file_size_left < size:
+                    form_str += '%s%s\r\n' % (start, content)
+                    self.file_size_left = 0
+                    size -= len(file_content)
+                    self.files_keys.remove(key)
+                else:
+                    form_str += '%s%s' % (start, content)
+                    self.file_size_left -= size
+                    size -= len(file_content)
+
+        return form_str
+
+    def read(self, size=None, loop=False):
+        if not self.files_keys and not self.form_str:
+            self.refresh()
+            if loop:
+                raise StopIteration
+            else:
+                return b''
+
+        if size is None:
+            size = sys.maxsize
 
         if self.form_str:
             form_str = self.form_str[:size]
@@ -69,52 +128,12 @@ class FileFormInputStream:
             form_str += '--{}--\r\n'.format(self.boundary)
         return form_str.encode('utf-8')
 
-    def file_str(self, size):
-        # handle file object
-        form_str = ''
-        start_fmt = '--%s\r\nContent-Disposition: form-data; name="%s";'
-        content_fmt = ' filename="%s"\r\nContent-Type: %s\r\n\r\n%s'
+    def refresh_cursor(self):
+        for ff in self.files.values():
+            ff.content.seek(0, 0)
 
-        if self.file_size_left:
-            for key in self.files_keys[:]:
-                if size <= 0:
-                    break
-                file_field = self.files[key]
-                file_content = file_field.content.read(self.size)
-                if isinstance(file_content, bytes):
-                    file_content = file_content.decode('utf-8')
-                if self.file_size_left <= size:
-                    form_str += '%s\r\n' % file_content
-                    self.file_size_left = 0
-                    size -= len(file_content)
-                    self.files_keys.remove(key)
-                else:
-                    form_str += file_content
-                    self.file_size_left -= self.size
-                    size -= len(file_content)
-        else:
-            for key in self.files_keys[:]:
-                if size <= 0:
-                    break
-                file_field = self.files[key]
-                file_size = os.path.getsize(file_field.content.name)
-                self.file_size_left = file_size
-                file_content = file_field.content.read(self.size)
-                if isinstance(file_content, bytes):
-                    file_content = file_content.decode('utf-8')
-
-                # build form_str
-                start = start_fmt % (self.boundary, key)
-                content = content_fmt % (
-                    file_field.filename, file_field.content_type, file_content)
-                if self.file_size_left < size:
-                    form_str += '%s%s\r\n' % (start, content)
-                    self.file_size_left = 0
-                    self.size -= len(file_content)
-                    self.files_keys.remove(key)
-                else:
-                    form_str += '%s%s' % (start, content)
-                    self.file_size_left -= self.size
-                    size -= len(file_content)
-
-        return form_str
+    def refresh(self):
+        self.file_size_left = 0
+        self._to_map()
+        self._build_str_forms()
+        self.refresh_cursor()
--- python/tests/test_client.py
+++ python/tests/test_client.py
@@ -2,6 +2,12 @@ import unittest
 from alibabacloud_tea_fileform.client import Client
 from alibabacloud_tea_fileform.models import FileField
 
+file1 = 'tests/test_file.json'
+file2 = 'tests/test.txt'
+
+# file1 = 'test_file.json'
+# file2 = 'test.txt'
+
 
 class TestClient(unittest.TestCase):
     def test_get_boundary(self):
@@ -9,35 +15,41 @@ class TestClient(unittest.TestCase):
         self.assertEqual(14, len(boundary))
 
     def test_to_file_from(self):
+        # Test 1
         body = Client.to_file_from({}, 'boundary')
         for i in body:
             self.assertEqual('--boundary--\r\n'.encode(), i)
 
         form = {
-            'stringkey': 'string'
+            'stringkey1': 'string1',
+            'stringkey2': 'string2'
         }
         body = Client.to_file_from(form, 'boundary')
         content = "--boundary\r\n" + \
-                  "Content-Disposition: form-data; name=\"stringkey\"\r\n\r\n" + \
-                  "string\r\n" + \
-                  "" \
+                  "Content-Disposition: form-data; name=\"stringkey1\"\r\n\r\n" + \
+                  "string1\r\n" + \
+                  "--boundary\r\n" + \
+                  "Content-Disposition: form-data; name=\"stringkey2\"\r\n\r\n" + \
+                  "string2\r\n" + \
                   "--boundary--\r\n"
         form_str = b''
         for i in body:
             form_str += i
         self.assertEqual(content.encode(), form_str)
+        # length: 86
         self.assertEqual(len(content.encode()), body.__len__())
 
-        f = open('test_file.json', encoding='utf-8')
-        file_field = FileField(
+        # # Test 2
+        f1 = open(file1, encoding='utf-8')
+        file_field1 = FileField(
             filename='test_file.json',
             content_type='application/json',
-            content=f
+            content=f1
         )
         boundary = Client.get_boundary()
         form = {
             'stringkey': 'string',
-            'filefield': file_field
+            'filefield': file_field1
         }
         body = Client.to_file_from(form, boundary)
 
@@ -55,9 +67,57 @@ class TestClient(unittest.TestCase):
         form_str = b''
         for i in body:
             form_str += i
-        f.close()
+
         self.assertEqual(
             content.encode('utf-8'),
             form_str
         )
+        # length: 247
         self.assertEqual(len(content.encode()), body.__len__())
+
+        # Test 3
+        f2 = open(file2, 'rb')
+        file_field2 = FileField(
+            filename='test.txt',
+            content_type='application/json',
+            content=f2
+        )
+        form = {
+            'stringkey': 'string',
+            'filefield1': file_field1,
+            'filefield2': file_field2
+        }
+        body = Client.to_file_from(form, boundary)
+
+        content = f"--{boundary}\r\n" + \
+                  "Content-Disposition: form-data; name=\"stringkey\"\r\n\r\n" + \
+                  "string\r\n" + \
+                  f"--{boundary}\r\n" + \
+                  "Content-Disposition: form-data; name=\"filefield1\"; filename=\"test_file.json\"\r\n" + \
+                  "Content-Type: application/json\r\n" + \
+                  "\r\n" + \
+                  "{\"test\": \"tests1\"}" + \
+                  "\r\n" + \
+                  f"--{boundary}\r\n" + \
+                  "Content-Disposition: form-data; name=\"filefield2\"; filename=\"test.txt\"\r\n" + \
+                  "Content-Type: application/json\r\n" + \
+                  "\r\n" + \
+                  "test1test2test3test4" + \
+                  "\r\n" + \
+                  f"--{boundary}--\r\n"
+        form_str = b''
+        for i in body:
+            form_str += i
+        self.assertEqual(content.encode(), form_str)
+        self.assertEqual(len(content.encode()), body.__len__())
+        form_str = b''
+        while True:
+            r = body.read(1)
+            if r:
+                form_str += r
+            else:
+                break
+        self.assertEqual(content.encode(), form_str)
+        self.assertEqual(len(content.encode()), body.__len__())
+        f1.close()
+        f2.close()

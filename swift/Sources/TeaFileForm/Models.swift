dafc0ceeb025a3369757e6e41abc44538106c5c8
yndu13 <yndu13@gmail.com>
2024-05-16 17:36:03 +0800

    refactor: modify read method for FileForm stream

--- swift/Sources/TeaFileForm/Models.swift
+++ swift/Sources/TeaFileForm/Models.swift
@@ -49,3 +49,123 @@ public class FileField : Tea.TeaModel {
         }
     }
 }
+
+class FileForm: InputStream {
+    private var form: [String: Any]
+    private var boundary: String
+    private var keys: [String]
+    private var keyIndex = 0
+    private var fileNumber = 0
+    private var files: [FileField] = []
+    private var fileBodyStream: InputStream?
+    private var temporaryStream: InputStream?
+    private var temporaryEndStream: InputStream?
+    private var endInputStream: InputStream?
+    
+    public init(_ form: [String: Any], _ boundary: String) {
+        self.form = form
+        self.boundary = boundary
+        self.keys = Array(form.keys)
+        self.keys = self.keys.sorted()
+        super.init(data: Data())
+        self.endInputStream = InputStream(data: "--\(boundary)--\r\n".data(using: .utf8)!)
+        endInputStream?.open()
+        prepareNextPart()
+    }
+
+    override func read(_ buffer: UnsafeMutablePointer<UInt8>, maxLength len: Int) -> Int {
+        fileBodyStream?.open()
+        defer {
+            fileBodyStream?.close()
+        }
+        var bytesRead = fileBodyStream?.read(buffer, maxLength: len) ?? 0
+        if bytesRead > 0 { return bytesRead }
+        
+        temporaryStream?.open()
+        defer {
+            temporaryStream?.close()
+        }
+        bytesRead = temporaryStream?.read(buffer, maxLength: len) ?? 0
+        if bytesRead > 0 { return bytesRead }
+        
+        temporaryEndStream?.open()
+        defer {
+            temporaryEndStream?.close()
+        }
+        bytesRead = temporaryEndStream?.read(buffer, maxLength: len) ?? 0
+        if bytesRead > 0 { return bytesRead }
+        
+        if fileNumber > 0 || keyIndex < keys.count {
+            prepareNextPart()
+            return read(buffer, maxLength: len)
+        }
+
+        if endInputStream?.hasBytesAvailable == true {
+            return endInputStream!.read(buffer, maxLength: len)
+        }
+        
+        return -1
+    }
+
+    override var hasBytesAvailable: Bool {
+        if keys.count <= 0 {
+            return false
+        }
+        return fileBodyStream?.hasBytesAvailable ?? false ||
+               temporaryStream?.hasBytesAvailable ?? false ||
+               temporaryEndStream?.hasBytesAvailable ?? false ||
+               endInputStream?.hasBytesAvailable ?? false ||
+               keyIndex < keys.count || fileNumber > 0
+    }
+    
+    override open func close() {
+        fileBodyStream?.close()
+        temporaryStream?.close()
+        temporaryEndStream?.close()
+        endInputStream?.close()
+        
+    }
+    
+    private func prepareNextPart() {
+        guard keyIndex < keys.count else {
+            if fileNumber > 0 {
+                prepareFilePart()
+            }
+            return
+        }
+        
+        let key = keys[keyIndex]
+        let value = form[key]!
+        
+        if let fileField = value as? FileField {
+            files.append(fileField)
+            fileNumber += 1
+            keyIndex += 1
+            prepareFilePart()
+        } else {
+            let contentString = "--\(boundary)\r\nContent-Disposition: form-data; name=\"\(key)\"\r\n\r\n\(value)\r\n"
+            fileBodyStream?.close()
+            fileBodyStream = InputStream(data: contentString.data(using: .utf8)!)
+            keyIndex += 1
+        }
+    }
+    
+    private func prepareFilePart() {
+        guard fileNumber > 0 else {
+            return
+        }
+        
+        let fileField = files.removeLast()
+        fileNumber -= 1
+        if fileField.content != nil {
+            let fileHeaderString = "--\(boundary)\r\nContent-Disposition: form-data; name=\"file\"; filename=\"\(fileField.filename!)\"\r\nContent-Type: \(fileField.contentType!)\r\n\r\n"
+            fileBodyStream?.close()
+            fileBodyStream = InputStream(data: fileHeaderString.data(using: .utf8)!)
+            temporaryStream?.close()
+            temporaryStream = fileField.content
+            temporaryEndStream?.close()
+            temporaryEndStream = InputStream(data: "\r\n".data(using: .utf8)!)
+        }
+    }
+
+}
--- swift/Tests/TeaFileFormTests/TeaFileFormTests.swift
+++ swift/Tests/TeaFileFormTests/TeaFileFormTests.swift
@@ -6,46 +6,44 @@ final class TeaFileFormTests: XCTestCase {
 
     func testFileFromStream() {
         let boundary: String = Client.getBoundary()
-        let stream: TeaFileForm = Client.toFileForm([String: Any](), boundary)
-
-        XCTAssertEqual(0, stream.bytes.count)
-        stream.write(boundary.toBytes())
-        XCTAssertEqual(32, stream.bytes.count)
+        XCTAssertEqual(32, boundary.toBytes().count)
+        let stream: InputStream = Client.toFileForm([String: Any](), boundary)
+        XCTAssertFalse(stream.hasBytesAvailable)
     }
 
     func testRead() {
         let fileField = FileField()
-        fileField.filename = "haveContent"
-        fileField.contentType = "contentType"
+        fileField.filename = "filename"
+        fileField.contentType = "text/plain; charset=utf-8"
         fileField.content = InputStream(data: Data("This is file test. This sentence must be long".toBytes()))
 
         let fileFieldNoContent = FileField()
-        fileFieldNoContent.filename = "noContent"
-        fileFieldNoContent.contentType = "contentType"
+        fileFieldNoContent.filename = "no-content"
+        fileFieldNoContent.contentType = "text/plain; charset=utf-8"
         fileFieldNoContent.content = nil
 
         let dict: [String: Any] = [
             "key": "value",
             "testKey": "testValue",
-            "haveFile": fileField,
+            "file": fileField,
             "noFile": fileFieldNoContent
         ]
 
-        let stream = Client.toFileForm(dict, "testBoundary")
+        let fileFormStream = Client.toFileForm(dict, "boundary")
 
-        var readLength: Int = 0
+        let bufferSize = 1024
+        var buffer = [UInt8](repeating: 0, count: bufferSize)
+        var totalData = Data()
+        while fileFormStream.hasBytesAvailable {
+            let bytesRead = fileFormStream.read(&buffer, maxLength: bufferSize)
+            if bytesRead <= 0 {
+                break
+            }
+            totalData.append(buffer, count: bytesRead)
+        }
 
-        repeat {
-            readLength = stream.read(0, 1024)
-        } while readLength != 0
-
-        let result: String = String(bytes: stream.bytes, encoding: .utf8) ?? ""
-        let target: String = "--testBoundary\r\nContent-Disposition: form-data; name=\"haveFile\"; filename=haveContent\r\nContent-Type: contentType\r\n\r\nThis is file test. This sentence must be long\r\n--testBoundary\r\nContent-Disposition: form-data; name=\"key\"\r\n\r\nvalue\r\n\r\n\r\n\r\n--testBoundary\r\nContent-Disposition: form-data; name=\"testKey\"\r\n\r\ntestValue\r\n\r\n\r\n--testBoundary--\r\n"
+        let result: String = String(data: totalData, encoding: .utf8) ?? ""
+        let target: String = "--boundary\r\nContent-Disposition: form-data; name=\"file\"; filename=\"filename\"\r\nContent-Type: text/plain; charset=utf-8\r\n\r\nThis is file test. This sentence must be long\r\n--boundary\r\nContent-Disposition: form-data; name=\"key\"\r\n\r\nvalue\r\n--boundary\r\nContent-Disposition: form-data; name=\"testKey\"\r\n\r\ntestValue\r\n--boundary--\r\n"
         XCTAssertEqual(target, result)
     }
-}
-
-class TestObject {
-    var name: String = ""
-    var value: Int = 0
 }
